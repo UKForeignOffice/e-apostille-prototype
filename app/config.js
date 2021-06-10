17795f01acce030d0988ddafa750055ef2884309
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2021-06-10 10:01:49 +0100

    Fix app title

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Get document legalised',
+  serviceName: 'Get a document legalised',
 
   // Default port that prototype runs on
   port: '3000',
