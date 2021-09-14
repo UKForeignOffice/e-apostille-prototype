b5250cc05fff023a182d5c187855330b84884733
dan-ashton-cyb <84323800+dan-ashton-cyb@users.noreply.github.com>
2021-09-14 09:58:50 +0100

    Updated verify journey - removed PDF/Paper question and updated content

--- app/routes/router-verify.js
+++ app/routes/router-verify.js
@@ -2,40 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-router.post('/WhatTypeOfApostille/form-handler', (req, res, next) => {
-  // Get the answer from session data
-  // The name between the quotes is the same as the 'name' attribute on the input elements
-  // However in JavaScript we can't use hyphens in variable names
+router.post('/VerifyApostille/form-handler', (req, res, next) => {
 
-  const apostilleType = req.session.data['typeofapostille']
-
-  if (apostilleType === 'epostille') {
-    res.redirect('/verify/VerifyEApostille')
-  } else {
-    res.redirect('/verify/VerifyPaper')
-  }
-})
-
-router.post('/VerifyEApostille/form-handler', (req, res, next) => {
-
-  const vcode = req.session.data['verificationcode']
-
-  if (vcode === 'ABCD-1234-EFGH-5678') {
-    res.redirect('/verify/EApostilleVerified')
-  } else {
-    res.redirect('/verify/EApostilleVerified')
-  }
-})
-
-
-router.post('/VerifyPaper/form-handler', (req, res, next) => {
-
-  const vcode = req.session.data['apostillenumber']
-
-  if (vcode === 'APO-123') {
+  const apostillenumber = req.session.data['apostillenumber']
+  if (apostillenumber === 'APO-1234567') {
     res.redirect('/verify/PaperVerified')
   } else {
-    res.redirect('/verify/PaperVerified')
+    res.redirect('/verify/EApostilleVerified')
   }
 })
 
