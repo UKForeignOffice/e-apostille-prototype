0a15f2243fd562b03eba6020a2802500ffd2b27f
Richard Oliver Bray <richardbray89@gmail.com>
2022-01-13 17:13:27 +0000

    feat: start adding pages for public beta

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // Add your routes here - above the module.exports line
-const multer = require('multer')
+const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: 'uploads/',
   filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-})
+    cb(null, file.originalname);
+  },
+});
 
-const upload = multer({ storage })
-const UPLOADS_PATH = '/application/7-upload-documents/form-handler'
+const upload = multer({ storage });
+const UPLOADS_PATH = '/application/7-upload-documents/form-handler';
 const COST_PER_PDF = 30;
 
 router.post(UPLOADS_PATH, upload.array('documents'), (req, res) => {
@@ -20,7 +20,7 @@ router.post(UPLOADS_PATH, upload.array('documents'), (req, res) => {
   req.session.data.documents = documentNames(req.body.documents);
   req.session.data.noOfDocs = req.session.data.documents.length;
   res.redirect('/application/8-user-reference');
-})
+});
 
 /**
  * @param {string | Array<string>} documents
@@ -38,62 +38,80 @@ function totalApplicationCost(documents) {
  * @returns {Array<string>}
  */
 function documentNames(documents) {
-  const onlyOneDocument = typeof documents === "string";
+  const onlyOneDocument = typeof documents === 'string';
 
   return onlyOneDocument ? [documents] : documents;
 }
 
-router.post('/application/sign-in/form-handler', (req, res, next) => {
+router.post('/application/1-who-are-you-answer', (req, res) => {
+  const whoAreYou = req.session.data['who-are-you'];
 
-  res.redirect('/application/2-your-account')
+  if (whoAreYou === 'new-user') {
+    return res.redirect('/application/2-search-for-documents');
+  }
+
+  res.redirect('/error-pages/generic');
 });
 
-router.post('/application/3-which-service/form-handler', (req, res, next) => {
+router.post('/application/sign-in/form-handler', (_req, res) => {
+  res.redirect('/application/2-your-account');
+});
+
+router.post('/application/3-which-service/form-handler', (req, res) => {
   if (req.session.data['service'] === 'standard-service') {
     res.redirect('/application/standard-service-document-check');
-    return
+    return;
   }
 
   if (req.session.data['service'] === 'premium-service') {
     res.redirect('/application/standard-service-document-check');
-    return
+    return;
   }
 
-  res.redirect("/application/4a-check-acceptance");
+  res.redirect('/application/4a-check-acceptance');
 });
 
-router.post('/application/4a-check-acceptance/form-handler', (req, res, next) => {
+router.post('/application/4a-check-acceptance/form-handler', (req, res) => {
   if (req.session.data['eapostille-acceptable'] === 'yes') {
-    res.redirect("/application/4-check-documents");
-    return
+    res.redirect('/application/4-check-documents');
+    return;
   }
-  res.redirect('/application/4a-check-acceptance-fail')
+  res.redirect('/application/4a-check-acceptance-fail');
 });
 
-router.post('/application/4-check-documents/form-handler', (req, res, next) => {
+router.post('/application/4-check-documents/form-handler', (req, res) => {
   if (req.session.data['documents-eligible'] === 'yes') {
-    res.redirect("/application/5-check-notarised-and-signed");
-    return
+    res.redirect('/application/5-check-notarised-and-signed');
+    return;
   }
-  res.redirect('/application/4-check-documents-fail')
+  res.redirect('/application/4-check-documents-fail');
 });
 
-router.post('/application/5-check-notarised-and-signed/form-handler', (req, res, next) => {
+router.post('/application/5-check-notarised-and-signed/form-handler', (req, res) => {
   if (req.session.data['notarised-and-signed'] === 'yes') {
-    res.redirect("/application/6-start-eapostille");
-    return
+    res.redirect('/application/6-start-eapostille');
+    return;
   }
-  res.redirect("/application/5-check-notarised-and-signed-fail");
+  res.redirect('/application/5-check-notarised-and-signed-fail');
 });
 
-router.post('/verify/VerifyApostille/form-handler', (req, res, next) => {
-
-  const apostillenumber = req.session.data['apostillenumber']
+router.post('/verify/VerifyApostille/form-handler', (req, res) => {
+  const apostillenumber = req.session.data['apostillenumber'];
   if (apostillenumber === 'APO-1234567') {
-    res.redirect('/verify/PaperVerified')
+    res.redirect('/verify/PaperVerified');
   } else {
-    res.redirect('/verify/EApostilleVerified')
+    res.redirect('/verify/EApostilleVerified');
   }
-})
+});
 
-module.exports = router
+router.post('/application/document-search-answer', (req, res) => {
+  const searchResults = req.session.data.search;
+
+  if (searchResults === 'degree certificate') {
+    return res.redirect('/application/2a-search-for-documents-degree');
+  }
+
+  res.redirect('/error-pages/generic');
+});
+
+module.exports = router;
